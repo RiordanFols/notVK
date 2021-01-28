25044d30382d9972147edddc4ea5449c8c3bbbe7
Pavel Chernov <riordanfols@gmail.com>
2021-01-28 15:40:47 +0300

    Migrations ids fix

--- src/main/resources/db/migration/V1__InitDB.sql
+++ src/main/resources/db/migration/V1__InitDB.sql
@@ -1,3 +1,7 @@
+alter table if exists comment_like
+    drop constraint if exists FKeh0kvj98rlb19mbg8ooifb9be;
+alter table if exists comment_like
+    drop constraint if exists FKqlv8phl1ibeh0efv4dbn3720p;
 alter table if exists message
     drop constraint if exists FK70bv6o4exfe3fbrho7nuotopf;
 alter table if exists message
@@ -15,6 +19,8 @@ alter table if exists user_subscriptions
 alter table if exists user_subscriptions
     drop constraint if exists FKm69uaasbua17sgdnhsq10yxd5;
 
+drop table if exists comment cascade;
+drop table if exists comment_like cascade;
 drop table if exists message cascade;
 drop table if exists post cascade;
 drop table if exists post_like cascade;
@@ -26,32 +32,6 @@ drop sequence if exists hibernate_sequence;
 
 create sequence hibernate_sequence start 1 increment 1;
 
-create table message
-(
-    id                 int8          not null,
-    creation_date_time timestamp     not null,
-    text               varchar(5000) not null,
-    user_id            int8          not null,
-    target_id          int8          not null,
-    primary key (id)
-);
-
-create table post
-(
-    id                 int8          not null,
-    creation_date_time timestamp     not null,
-    text               varchar(5000) not null,
-    user_id            int8          not null,
-    primary key (id)
-);
-
-create table post_like
-(
-    post_id int8 not null,
-    user_id int8 not null,
-    primary key (post_id, user_id)
-);
-
 create table comment
 (
     id                 int8          not null,
@@ -61,28 +41,47 @@ create table comment
     post_id            int8          not null,
     primary key (id)
 );
-
 create table comment_like
 (
     comment_id int8 not null,
     user_id    int8 not null,
     primary key (comment_id, user_id)
 );
-
+create table message
+(
+    id                 int8          not null,
+    creation_date_time timestamp     not null,
+    text               varchar(5000) not null,
+    user_id            int8          not null,
+    target_id          int8          not null,
+    primary key (id)
+);
+create table post
+(
+    id                 int8          not null,
+    creation_date_time timestamp     not null,
+    text               varchar(5000) not null,
+    user_id            int8          not null,
+    primary key (id)
+);
+create table post_like
+(
+    post_id int8 not null,
+    user_id int8 not null,
+    primary key (post_id, user_id)
+);
 create table user_role
 (
     user_id int8         not null,
     role    varchar(255) not null,
     primary key (user_id, role)
 );
-
 create table user_subscriptions
 (
     subscriber_id int8 not null,
     target_id     int8 not null,
     primary key (target_id, subscriber_id)
 );
-
 create table usr
 (
     id        int8         not null,
@@ -93,33 +92,27 @@ create table usr
     username  varchar(255) not null,
     primary key (id)
 );
-
-alter table if exists message
-    add constraint FK70bv6o4exfe3fbrho7nuotopf foreign key (user_id) references usr;
-
-alter table if exists message
-    add constraint FKc3jkt36cxuic4o8eubtfau5w6 foreign key (target_id) references usr;
-
-alter table if exists post
-    add constraint FKrm2u0ujvvi9euawhsm1km29m4 foreign key (user_id) references usr;
-
-alter table if exists post_like
-    add constraint FK9j3wh3dvfv4flpe7ouv6qtcau foreign key (user_id) references usr;
-
-alter table if exists post_like
-    add constraint FKj7iy0k7n3d0vkh8o7ibjna884 foreign key (post_id) references post;
-
-alter table if exists user_role
-    add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr;
-
-alter table if exists user_subscriptions
-    add constraint FK4yw84gj44ckwb4cbg7kmrkai5 foreign key (target_id) references usr;
-
-alter table if exists user_subscriptions
-    add constraint FKm69uaasbua17sgdnhsq10yxd5 foreign key (subscriber_id) references usr;
-
+alter table if exists comment
+    add constraint FKgcgdcgly6u49hf4g8y2di3g4p foreign key (user_id) references usr;
+alter table if exists comment
+    add constraint FKs1slvnkuemjsq2kj4h3vhx7i1 foreign key (post_id) references post on delete cascade;
 alter table if exists comment_like
     add constraint FKeh0kvj98rlb19mbg8ooifb9be foreign key (user_id) references usr;
-
 alter table if exists comment_like
-    add constraint FKqlv8phl1ibeh0efv4dbn3720p foreign key (comment_id) references comment;
+    add constraint FKqlv8phl1ibeh0efv4dbn3720p foreign key (comment_id) references comment on delete cascade;
+alter table if exists message
+    add constraint FK70bv6o4exfe3fbrho7nuotopf foreign key (user_id) references usr;
+alter table if exists message
+    add constraint FKc3jkt36cxuic4o8eubtfau5w6 foreign key (target_id) references usr;
+alter table if exists post
+    add constraint FKrm2u0ujvvi9euawhsm1km29m4 foreign key (user_id) references usr;
+alter table if exists post_like
+    add constraint FK9j3wh3dvfv4flpe7ouv6qtcau foreign key (user_id) references usr;
+alter table if exists post_like
+    add constraint FKj7iy0k7n3d0vkh8o7ibjna884 foreign key (post_id) references post on delete cascade;
+alter table if exists user_role
+    add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr;
+alter table if exists user_subscriptions
+    add constraint FK4yw84gj44ckwb4cbg7kmrkai5 foreign key (target_id) references usr;
+alter table if exists user_subscriptions
+    add constraint FKm69uaasbua17sgdnhsq10yxd5 foreign key (subscriber_id) references usr
