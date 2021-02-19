9bd38adc6a9316a0de13638e04bd6a452fa54bf6
Pavel Chernov <riordanfols@gmail.com>
2021-02-19 20:17:55 +0300

    Last online gender for verb

--- src/main/java/ru/chernov/notvk/formatter/UserInfoFormatter.java
+++ src/main/java/ru/chernov/notvk/formatter/UserInfoFormatter.java
@@ -3,6 +3,7 @@ package ru.chernov.notvk.formatter;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.chernov.notvk.domain.Gender;
 import ru.chernov.notvk.domain.entity.User;
 import ru.chernov.notvk.utils.DateUtils;
 
@@ -18,11 +19,15 @@ import java.time.ZoneOffset;
 @Getter
 public class UserInfoFormatter {
 
+    private final String VERB_MALE = "был";
+    private final String VERB_FEMALE = "была";
+    private final String VERB_UNDEFINED = "был(а)";
+
     private final String ONLINE_MESSAGE = "онлайн";
-    private final String MINUTES_AGO_MESSAGE = "был(а) в сети %s минут назад";
-    private final String HOURS_AGO_MESSAGE = "был(а) в сети %s часов назад";
-    private final String DAYS_AGO_MESSAGE = "был(а) в сети %s дней назад";
-    private final String LAST_DATE_MESSAGE = "был(а) в сети %s";
+    private final String MINUTES_AGO_MESSAGE =" в сети %s минут назад";
+    private final String HOURS_AGO_MESSAGE = " в сети %s часов назад";
+    private final String DAYS_AGO_MESSAGE = " в сети %s дней назад";
+    private final String LAST_DATE_MESSAGE = " в сети %s";
 
     @Value("${session.threshold.seconds}")
     private int secondsThreshold;
@@ -40,36 +45,49 @@ public class UserInfoFormatter {
         var now = LocalDateTime.now();
         var lastOnline = user.getLastOnline();
 
+        String lastOnlineString = "";
+
         // сейчас онлайн
         int secondsAgo = (int)(now.toEpochSecond(ZoneOffset.UTC) - lastOnline.toEpochSecond(ZoneOffset.UTC));
         if (secondsAgo < secondsThreshold) {
-            user.setLastOnlineString(ONLINE_MESSAGE);
+            lastOnlineString = ONLINE_MESSAGE;
+            user.setLastOnlineString(lastOnlineString);
             return;
+        }
+
+        switch (user.getGender()) {
+            case MALE -> lastOnlineString += VERB_MALE;
+            case FEMALE -> lastOnlineString += VERB_FEMALE;
+            case UNDEFINED -> lastOnlineString += VERB_UNDEFINED;
         }
 
         // был в сети n минут назад
         int minutesAgo = secondsAgo / 60;
         if (minutesAgo < minutesThreshold) {
-            user.setLastOnlineString(String.format(MINUTES_AGO_MESSAGE, minutesAgo));
+            lastOnlineString += String.format(MINUTES_AGO_MESSAGE, minutesAgo);
+            user.setLastOnlineString(lastOnlineString);
             return;
         }
 
         // был в сети n часов назад
         int hoursAgo = minutesAgo / 60;
         if (hoursAgo < hoursThreshold) {
-            user.setLastOnlineString(String.format(HOURS_AGO_MESSAGE, hoursAgo));
+            lastOnlineString += String.format(HOURS_AGO_MESSAGE, hoursAgo);
+            user.setLastOnlineString(lastOnlineString);
             return;
         }
 
         // был в сети n часов назад
         int daysAgo = hoursAgo / 24;
         if (daysAgo < daysThreshold) {
-            user.setLastOnlineString(String.format(DAYS_AGO_MESSAGE, daysAgo));
+            lastOnlineString += String.format(DAYS_AGO_MESSAGE, daysAgo);
+            user.setLastOnlineString(lastOnlineString);
             return;
         }
 
         // показ полной даты последнего онлайна
-        user.setLastOnlineString(String.format(LAST_DATE_MESSAGE, DateUtils.formatDate(lastOnline)));
+        lastOnlineString += String.format(LAST_DATE_MESSAGE, DateUtils.formatDate(lastOnline));
+        user.setLastOnlineString(lastOnlineString);
     }
 
     public void formatBirthdayString(User user) {
